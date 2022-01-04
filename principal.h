e42856afdf28d5c50f7f6a153fc58737e6d37ed9
Andrewrv43 <paul.rueda@hotmail.com>
2022-01-04 08:20:24 -0500

    Conversor V1.1

---
+++ principal.h
@@ -0,0 +1,29 @@
+#ifndef PRINCIPAL_H
+#define PRINCIPAL_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Principal; }
+QT_END_NAMESPACE
+
+class Principal : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Principal(QWidget *parent = nullptr);
+    ~Principal();
+public slots:
+    void cent2fahr(int i);
+    void fahr2cent(int i);
+    void cent2kelv(int i);
+    void kelv2cent(int i);
+    void fahr2kelv(int i);
+    void kelv2fahr(int i);
+
+
+private:
+    Ui::Principal *ui;
+};
+#endif // PRINCIPAL_H
